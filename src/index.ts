93f41b001838dab182831a77fc678ae6e9961254
Saeed Badran <salouri@gmail.com>
2022-07-17 15:40:57 +0300

    add bookRouter router to index.ts (app)

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import { authorRouter } from './author/author.router';
+import { bookRouter } from './book/book.router';
 
 
 const PORT: number = parseInt(process.env.PORT || '8000', 10);
@@ -9,9 +10,10 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/authors', authorRouter);
+app.use('/api/books', bookRouter);
 
 
 app.listen(PORT, () => {
